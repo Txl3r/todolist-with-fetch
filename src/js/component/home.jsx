df44c9aba508b658b712b1a710de4f986abe0570
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-04-05 19:24:41 +0000

    made changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [todoList, setTodolist] = useState([]);
+	const line = (x) => {
+		const newList = todoList.filter((element, index) => index !== x);
+		setTodolist(newList);
+	};
 
 	useEffect(() => {
 		fetch("http://assets.breatheco.de/apis/fake/todos/user/harvey46", {
@@ -56,20 +60,41 @@ const Home = () => {
 		<div className="container">
 			<div className="input-group mb-3">
 				<input
-					type="text"
+					type="test"
 					className="form-control"
-					placeholder="Recipient's username"
-					aria-label="Recipient's username"
-					aria-describedby="basic-addon2"></input>
-				<div className="input-group-append">
-					<button
-						onClick={() => {}}
-						className="input-group-text"
-						id="basic-addon2">
-						Set Value
-					</button>
-				</div>
+					placeholder="add a task"
+					onChange={(e) => setItem(e.target.value)}
+					value={item}
+				/>
+				<button
+					onClick={() => {
+						if (item !== "") {
+							setTodolist([...todoList, item]);
+							setItem("");
+						}
+					}}
+					type="btn btn-secondary"
+					className="input-group-test"
+					id="basic-addon1">
+					Add Task
+				</button>
 			</div>
+			<ul>
+				{todoList.map((element, index) => {
+					return (
+						<li key={index} className="mr-2">
+							{element}
+							<a
+								className="btn btn-primary"
+								onClick={() => {
+									line(index);
+								}}>
+								x
+							</a>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
